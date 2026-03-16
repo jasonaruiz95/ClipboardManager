f03bf876dc3652cf42e5d286eb27308ec38f3613
Jasonruiz <jasonaruiz6@gmail.com>
2026-03-15 18:16:02 -0600

    Added data class library

---
+++ Data/ClipboardEntry.cs
@@ -0,0 +1,33 @@
+﻿namespace ClipboardManager.Models;
+
+
+public class ClipboardEntry
+{
+    public int Id { get; set; }
+    public DateTime Time { get; set; }
+    public string Text { get; set; }
+    public DateTime CopiedAt { get; set; } = DateTime.Now;
+
+    public ClipboardEntry(string text)
+    {
+        Text = text;
+        Time = DateTime.Now;
+    }
+
+    // public bool Equals(ClipboardEntry? other)
+    // {
+    //     if (other is null) return false;
+    //     return Text == other.Text;
+    //     
+    // }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is ClipboardEntry other)
+        {
+            return Text == other.Text;
+        }
+        return false;
+    }
+    
+}
