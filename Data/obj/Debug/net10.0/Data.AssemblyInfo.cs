76fed157fd697869981766f5d0e30e68ef244421
jasonruiz <jason.ruiz@melonlocal.com>
2026-03-15 19:39:14 -0600

    updating clipboard manager project file to include clipboard icon asset

--- Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e163273471081f35b0aea856a20d0418808545a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7f7d3054a0079c4dd4756e37ab4ae20c2aef04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
