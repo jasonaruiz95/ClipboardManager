3e163273471081f35b0aea856a20d0418808545a
Jasonruiz <jasonaruiz6@gmail.com>
2026-03-15 19:01:28 -0600

    Added icon and windows publish.

--- Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee58e015ccb379fd42bc68342ea2175f09bfd7d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cddd2a8d1e4a9e48c35120403f04df0029b75241")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
